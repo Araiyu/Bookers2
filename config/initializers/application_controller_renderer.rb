181adbf100f6d37e50ad017c324713148ecaa3a8
新井　雄介 <[ay088055@gmail.com]>
2019-06-17 17:26:00 +0900

    F-device

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '296d4fccbc0eae4022685937e5267cfce4dd16c282e006d9aec537e68d6d7f8493873026e0ce2d56692705c28983ead51fd6a5f34582d79918c2cb7bcda4ca11'
